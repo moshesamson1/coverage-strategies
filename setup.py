b26f83e8d8239425af4073b94fd9bf4063b1f05f
moshesamson1 <36891921+moshesamson1@users.noreply.github.com>
2020-04-16 12:03:24 +0300

    Update setup.py
    Add download link

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Samson Moshe',                   # Type in your name
   author_email = 'samson.moshe@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/moshesamson1',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/moshesamson1/coverage_strategies/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['COVERAGE', 'PATHS', 'SIMULATIONS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
