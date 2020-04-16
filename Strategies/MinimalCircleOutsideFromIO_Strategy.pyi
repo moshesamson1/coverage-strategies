6287552b563a3f0a3891b029115eb1a1d7a847a5
moshesamson1 <samson.moshe@gmail.com>
2020-04-16 11:51:59 +0300

    create init file

---
+++ Strategies/MinimalCircleOutsideFromIO_Strategy.pyi
@@ -0,0 +1,6 @@
+from Simulations_Thesis.Entities import Strategy
+
+
+class MinimalCircleOutsodeFromIO_Strategy(Strategy):
+    def get_steps(self, agent_r, board_size = 50, agent_o = None):
+        pass
---
+++ Strategies/STC_Strategy.py
@@ -0,0 +1,11 @@
+from Simulations_Thesis.Entities import Strategy, Slot
+from Simulations_Thesis import SpanningTreeCoverage
+
+
+class STC_Strategy(Strategy):
+    # @abstractmethod
+    def get_steps(self, agent_r, board_size = 50, agent_o = None):
+        return SpanningTreeCoverage.get_random_coverage_strategy(size=board_size,
+                                                                 i_r=Slot(agent_r.InitPosX, agent_r.InitPosY),
+                                                                 print_mst=False)
+
