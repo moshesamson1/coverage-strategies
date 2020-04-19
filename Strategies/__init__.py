867bdadcffb9676d70e3ae1fc346c2bf80d5bcfe
moshesamson1 <samson.moshe@gmail.com>
2020-04-19 11:52:06 +0300

    changing init files

--- Strategies/__init__.py
+++ Strategies/__init__.py
@@ -1,14 +1,30 @@
-__all__ = ["CircleOutsideFromIo_Strategy",
-           "VerticalCircularCoverage_Strategy",
-           "HorizontalCircularCoverage_Strategy",
-           "InterceptThenCopy_Strategy",
-           "CoverByQuarters_Strategy",
-           "STC_Strategy",
-           "VerticalNonCircularCoverage_Strategy",
-           "CircleInsideFromCornerFarthestFromIo_Strategy",
-           "CircleOutsideFromBoardCenter_Strategy",
-           "VerticalCoverageFromCornerFarthestFromIo_Strategy",
-           "CircleOutsideFromCornerFarthestFromIo_Strategy",
-           "LCP_Strategy",
-           "LongestToReach_Strategy",
-           "TrulyRandom_Strategy"]
+# __all__ = ["CircleOutsideFromIo_Strategy",
+#            "VerticalCircularCoverage_Strategy",
+#            "HorizontalCircularCoverage_Strategy",
+#            "InterceptThenCopy_Strategy",
+#            "CoverByQuarters_Strategy",
+#            "STC_Strategy",
+#            "VerticalNonCircularCoverage_Strategy",
+#            "CircleInsideFromCornerFarthestFromIo_Strategy",
+#            "CircleOutsideFromBoardCenter_Strategy",
+#            "VerticalCoverageFromCornerFarthestFromIo_Strategy",
+#            "CircleOutsideFromCornerFarthestFromIo_Strategy",
+#            "LCP_Strategy",
+#            "LongestToReach_Strategy",
+#            "TrulyRandom_Strategy"]
+
+from coverage_strategies.Strategies.CircleOutsideFromIo_Strategy import *
+from coverage_strategies.Strategies.VerticalCircularCoverage_Strategy import *
+from coverage_strategies.Strategies.HorizontalCircularCoverage_Strategy import *
+from coverage_strategies.Strategies.InterceptThenCopy_Strategy import *
+from coverage_strategies.Strategies.CoverByQuarters_Strategy import *
+from coverage_strategies.Strategies.STC_Strategy import *
+from coverage_strategies.Strategies.VerticalNonCircularCoverage_Strategy import *
+from coverage_strategies.Strategies.CircleInsideFromCornerFarthestFromIo_Strategy import *
+from coverage_strategies.Strategies.CircleOutsideFromBoardCenter_Strategy import *
+from coverage_strategies.Strategies.VerticalCoverageFromCornerFarthestFromIo_Strategy import *
+from coverage_strategies.Strategies.CircleOutsideFromCornerFarthestFromIo_Strategy import *
+from coverage_strategies.Strategies.LCP_Strategy import *
+from coverage_strategies.Strategies.LongestToReach_Strategy import *
+from coverage_strategies.Strategies.TrulyRandom_Strategy import *
+
